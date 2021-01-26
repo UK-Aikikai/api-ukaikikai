cba937878dd6c8c3afa1c053be5c48ac40098c7f
Antonios Pavlakis <antonios.pavlakis@staysafeapp.com>
2021-01-26 21:52:39 +0000

    Debug CI steps

--- scripts/generate.php
+++ scripts/generate.php
@@ -4,6 +4,8 @@ $rootDir = dirname(__DIR__, 1);
 
 require_once $rootDir . '/src/Convertor.php';
 
+echo 'root dir: ' . $rootDir . PHP_EOL;
+
 $convertor = new \UKAikikai\Videos\Api\Convertor($rootDir);
 $videoCollection = $convertor->fromCsvToArray();
 $convertor->saveAsJson($videoCollection);
